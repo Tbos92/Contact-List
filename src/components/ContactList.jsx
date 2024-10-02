5665a944a09fded3f0d244aeb16bd71ee115fb9e
Tyler <tbos92@gmail.com>
2024-10-01 20:57:55 -0400

    commit for API fetchContacts function added

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ContactRow from "./ContactRow";
 
 const dummyContacts = [
@@ -11,6 +11,20 @@ const dummyContacts = [
 export default function ContactList() {
   const [contacts, setContacts] = useState(dummyContacts);
   console.log("contacts: ", contacts);
+  useEffect(() => {
+    async function fetchContacts() {
+      try {
+        const response = await fetch(
+          "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users"
+        );
+        const result = await response.json();
+        setContacts(result);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchContacts();
+  }, []);
 
   return (
     <table>
