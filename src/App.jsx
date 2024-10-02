6673e177e57bff7dba375916d51068b2c729c1fa
Tyler <tbos92@gmail.com>
2024-10-01 21:45:40 -0400

    Committing Selected Contact View coding changes to App.jsx and ContactList.jsx/ContactRow.jsx components. Added SelectedContact.jsx component, I think I jumped thestart on that one

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { useState, useEffect } from "react";
 import "./App.css";
+import { useState } from "react";
 import ContactList from "./components/ContactList";
+import SelectedContact from "./components/SelectedContact";
 
-export function App() {
+export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
+
   return (
     <>
-      <ContactList />
+      {selectedContactId ? (
+        <div>Selected Contact View</div>
+      ) : (
+        <ContactList setSelectedContactId={setSelectedContactId} />
+      )}
     </>
   );
 }
-
-export default App;
